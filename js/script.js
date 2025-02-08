8c93effaece1b771f12504b5eb26c415eeadbdf6
Danieldandrade <dan.andrade20@gmail.com>
2025-02-08 06:35:33 -0300

    add subtotal

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-function adicionar(produto){
+function alterarQtd(produto, acao){
     const qtd = document.getElementById('qtd_' + produto)
     const valor = document.getElementById('valor_' + produto)
     const total = document.getElementById('total_' + produto)
 
-    qtd.innerHTML ++
-
-    total.innerHTML = qtd.innerHTML * valor.innerHTML
-
+    if(acao == '-' && qtd.innerHTML == 0){
+        alert('Atenção! A Quantidade nao pode ser menor que 0')
+    }else{   
+        acao == '+' ? qtd.innerHTML ++ : qtd.innerHTML-- ;
+        total.innerHTML = qtd.innerHTML * valor.innerHTML
+        soma()
+    }
 }
+function soma(){
 
-function retirar(produto){
-    const qtd = document.getElementById('qtd_' + produto)
-    const valor = document.getElementById('valor_' + produto)
-    const total = document.getElementById('total_' + produto)
+    let soma = 0
+    
+    soma = soma + Number(document.getElementById('total_1').innerHTML)
 
-    qtd.innerHTML --
+    soma = soma + Number(document.getElementById('total_2').innerHTML)
 
-    total.innerHTML = qtd.innerHTML * valor.innerHTML
+    soma = soma + Number(document.getElementById('total_3').innerHTML)
+
+    document.getElementById('subtotal').innerHTML = soma
 }
